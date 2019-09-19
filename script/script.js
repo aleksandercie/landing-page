01f582c414ca0e45d0e73500f8e7fc6e5e088fe9
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-09-19 15:39:06 +0200

    landing-page

---
+++ script/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Materhorn', 'Mount Everest', 'Ama Dablam'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1100,
+    controlArrows: false,
+    slidesNavigation: true,
+
+})
